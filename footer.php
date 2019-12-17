f1e3253b91596c4ec3ec5b81720d86ad2ec3104c
veronika-rnk <51095812+veronika-rnk@users.noreply.github.com>
2019-12-17 17:48:00 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+﻿
+<div id="footer">
+    
+<h2>Ждём именно вас!</h2>
+<?php
+echo date("F j, Y, g:i a"); 
+?>
+
+
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+﻿<?php 
+    
+//Подключение шапки
+    
+require_once("header.php");
+?>
+<div id="content">
+   
+<h2>Митап «JavaScript: Front End plus Back End»</h2>
+    
+<p>JavaScript программисты давно уже перестали быть просто создателями front end части сайтов и приложений.
+Появление Node.js значительно увеличило возможности веб-разработчиков, превратив их в fullstack девелоперов.
+Узнай о всем спектре перспектив для JavaScript разработчиков на митапе «JavaScript: Front End plus Back End»
+<p>
+В программе митапа 2 доклада:
+</p>
+<dl>
+<dt>1) Dog eat dog world</dt>
+<ul type = "disc">
+<dd>цель самообучения и курсов</dd>
+<dd>источники опыта</dd>
+<dd>собеседование и все что нужно знать об этом JavaScript инженеру</dd>
+</dl>
+<p>Спикер: Олег Момот — JavaScript software engineer, Community contributor, Technical reviewer, Lecturer, Mentor, более 3-х лет опыта в сфере IT.</p>
+<dl>
+<dt>2) Node.js и перспективы</dt>
+<ul type = "disc">
+<dd>сфера применения Node.js</dd>
+<dd>откуда берется потребность в Node.js-программистах: цифровая трансформация бизнеса</dd>
+<dd>варианты развития после изучения технологии</dd>
+
+<p>Спикер: Артур Кашаев — JavaScript software developer, более 6 лет в разработке. Работает с технологиями AngularJS 1.x, Ember 2.0, VueJS, NodeJS, Postgresql, Redis, Mocha, Chai, Sinon, Bootstrap и др.</p>
+
+<p>Митап бесплатный. Тебе нужно всего лишь зарегистрироваться. Сделай это прямо сейчас — воспользуйся возможностью узнать больше о перспективной IT-профессии.</p>
+    
+</p>
+<p>Для посещения этого форума, пройдите регистрацию или авторизуйтесь.</p>
+
+</div>
+
+<h2>Фотогалерея</h2>
+<div block = "pict">
+<img src="Images/team2.jpg" alt="Тренинг">
+
+</div>
+
+<?php 
+
+
+
+    
+//Подключение подвала
+    
+require_once("footer.php");
+?>
